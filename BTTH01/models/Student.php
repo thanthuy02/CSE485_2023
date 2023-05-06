2431da7d5c7e42d8e94e8e61ce669c019c8aeb9f
uyennnnnn <buiuyen1207@gmail.com>
2023-05-06 11:03:05 +0700

    Tao lop student

---
+++ BTTH01/models/Student.php
@@ -0,0 +1,32 @@
+<!-- Tao lop student -->
+<?php
+class Student {
+    private $id;
+    private $name;
+    private $age;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+}
+?>
