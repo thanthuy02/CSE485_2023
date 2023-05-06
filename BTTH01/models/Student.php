ce1df9016da371ad47d565727c93dc0e9fb4c061
uyennnnnn <buiuyen1207@gmail.com>
2023-05-06 11:14:26 +0700

    Them grade vao lop student

--- BTTH01/models/Student.php
+++ BTTH01/models/Student.php
@@ -4,29 +4,46 @@ class Student {
     private $id;
     private $name;
     private $age;
-    
-    public function getId() {
-        return $this->id;
+    private $grade;
+
+    public function __construct($id, $name, $age, $grade) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->age = $age;
+        $this->grade = $grade;
     }
 
-    public function setId($id) {
-        $this->id = $id;
+    public function getId() {
+        return $this->id;
     }
 
     public function getName() {
         return $this->name;
     }
 
-    public function setName($name) {
-        $this->name = $name;
-    }
-
     public function getAge() {
         return $this->age;
+    }
+
+    public function getGrade() {
+        return $this->grade;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
     }
 
     public function setAge($age) {
         $this->age = $age;
     }
+
+    public function setGrade($grade) {
+        $this->grade = $grade;
+    }
 }
+
 ?>
