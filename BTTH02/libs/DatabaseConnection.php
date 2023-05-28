f4a3c083526a6fefa9483b69fadf15bed8cd1659
Thuy Than <2051063434@e.tlu.edu.vn>
2023-05-29 00:17:36 +0700

    resolve conflict

--- BTTH02/libs/DatabaseConnection.php
+++ BTTH02/libs/DatabaseConnection.php
@@ -1,19 +1,12 @@
 <?php
 require_once './config/config.php';
 class DatabaseConnection {
-<<<<<<< HEAD:BTTH02/DatabaseConnection.php
-    private $host = 'localhost';
-    private $dbname = 'attendance_management';
-    private $port     = '3306'; 
-    private $username = 'root';
-    private $password = 'thuy123';
-=======
+
     private $host;
     private $dbname;
     private $port; 
     private $username;
     private $password;
->>>>>>> 14af6b0841c4499e625b4efdd8fb4143d10c812a:BTTH02/libs/DatabaseConnection.php
     private $connection;
 
     public function __construct() {
